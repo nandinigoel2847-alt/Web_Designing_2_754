4137f69d694a5941048c36935c0408056f17c397
nandinigoel2847-alt <nandinigoel2847@gmail.com>
2026-05-17 09:37:01 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:"/nandinigoel2847-alt/Web_Designing_2_754",
+})
